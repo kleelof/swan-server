b63e8d29cefc6a3dc0e8633b0d9b55dd356f7f93
kleelof <kleelof@yahoo.com>
2020-02-22 15:47:59 -0800

    set CORS origins to all

--- src/SocketServer.ts
+++ src/SocketServer.ts
@@ -13,13 +13,14 @@ export class SocketServer {
 
     constructor(){
         this.httpConnection = createServer();
-        this.socketHandler = io(this.httpConnection);
+        this.socketHandler = io(this.httpConnection, {origins: "*"});
+        
     }
 
     public listen(PORT: number): Promise<string> {
         return new Promise((resolve, reject) => {
             this.socketHandler.on('connection', (socket: io.Socket) => {
-                
+                console.log('connection');
                 this.routes.forEach((route: SocketRoute) => {
                     socket.on(route.name, (args: any, fn) => {
                         route.view.processRequest(socket, args, fn);
